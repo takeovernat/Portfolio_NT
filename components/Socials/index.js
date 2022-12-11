3d8313ecc7be3df6aeb1437b0a3c2558c57a4eb8
Takeovernat <natnaelteshome9@gmail.com>
2022-12-10 18:37:44 -0600

    upload to repo

---
+++ components/Socials/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "../Button";
+
+import myData from "../../data/portfolio.json";
+
+const Socials = ({ className }) => {
+  return (
+    <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
+      {myData.socials.map((social, index) => (
+        <Button key={index} onClick={() => window.open(social.link)}>
+          {social.title}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default Socials;
